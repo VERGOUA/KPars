0e3fd46d6c63650c63fafa6085c45b074b2b75da
ReenExe <ipourri@gmail.com>
2015-10-26 23:09:25 +0200

    use limit

--- app/src/AppBundle/Command/FilmPageCommand.php
+++ app/src/AppBundle/Command/FilmPageCommand.php
@@ -4,19 +4,22 @@ namespace AppBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class FilmPageCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
-        $this->setName('kp:scrap:filp:page');
+        $this
+            ->setName('kp:scrap:filp:page')
+            ->addOption('limit', null, InputOption::VALUE_OPTIONAL);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $startTime = microtime(true);
-        $limit = 3;
+        $limit = (int) $input->getOption('limit') ?: 10;
 
         $this->getContainer()->get('kp.film_page_scrap')->scrap($limit);
 
